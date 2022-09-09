2c87818f81f7640c187a93eed4951c70530a3bfe
ipinmi <ipinmiadebayo@gmail.com>
2022-09-09 12:25:17 -0700

    variables and loops

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ *main - the entry point
+ *Descrition: print the alphabet in lowercase
+ *and skip some letters
+ *Return: always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			continue;
+		}
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
