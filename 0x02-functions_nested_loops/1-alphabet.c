55827512d1fd844baa46920b7b1fc664ec1e4b57
ipinmi <ipinmiadebayo@gmail.com>
2022-09-13 15:34:02 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *print_alphabet - entry point
+ *
+ *Description: prints the alphabet in lowercase
+ *
+ *Return: always zero
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a', i <= 'z', i++)
+		_putchar(i);
+	_putchar('\n');
+}
