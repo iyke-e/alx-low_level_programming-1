99f8fc6d76567d1a78736d1e0572d94d9aa2f6c9
ipinmi <ipinmiadebayo@gmail.com>
2022-09-13 18:24:16 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *add - main entry
+ *Description: adds two integers
+ *@n: integer
+ *@m: numbers to add
+ *Return: always zero
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
