de1a71491a7bc67f8ccd226f28222fcb070adbad
ipinmi <ipinmiadebayo@gmail.com>
2022-09-13 18:43:08 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *print_to_98 - main point
+ *Description: prints all natural numbers from n to 98
+ *@n: starting point
+ *Return: always zero
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	} else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
