6fe16c19a4b1fcc80a4d607a10ca9349a7081eba
ipinmi <ipinmiadebayo@gmail.com>
2022-09-13 17:51:37 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *jack_bauer - main entry
+ *Description: every minute of the day of Jack Bauer
+ *Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
