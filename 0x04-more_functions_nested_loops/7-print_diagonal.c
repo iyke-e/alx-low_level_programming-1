3b88c72fbe87c2ab419136ebcff5ea2f5a2f4c09
ipinmi <ipinmiadebayo@gmail.com>
2022-09-15 13:24:28 -0700

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - the entry point
+ * Description: draws a diagonal line in the terminal
+ * @n: number of slash
+ * Return: always 0
+ */
+void print_diagonal(int n)
+{
+	int slash;
+	int spaces;
+	int i;
+
+	for (slash = n; slash > 0; slash--)
+	{
+		spaces = 10 - slash;
+		for (i = spaces; i < spaces; i--)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+	}
+	_putchar('\n');
+}
