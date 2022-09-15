756466715e500df982fb611ecff0502ab052b73c
ipinmi <ipinmiadebayo@gmail.com>
2022-09-15 11:52:19 -0700

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - the entry point
+ * Description: prints the numbers, from 0 to 9 followed by a new line
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+}
