18ad7b4c66812562767659c5de338ca57ed80a71
ipinmi <ipinmiadebayo@gmail.com>
2022-09-15 11:42:38 -0700

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit - the entry point
+ *
+ * Description: checks for digits
+ *
+ * @c: letter to check
+ * Return: always 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
