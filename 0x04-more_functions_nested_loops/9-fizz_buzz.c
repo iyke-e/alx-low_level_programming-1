55e0aac0bff3e4d9dc97c66b275252ec85632e2a
ipinmi <ipinmiadebayo@gmail.com>
2022-09-15 15:24:53 -0700

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - the entry point
+ * Description: fizzbizz test
+ * Return: always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0 && (num % 5) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if ((num % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((num % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", num);
+		}
+	}
+	printf("\n");
+	return (0);
+}
