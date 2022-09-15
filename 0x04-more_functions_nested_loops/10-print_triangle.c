423abc4d7db812d12631aa1d2775cc19ebc57475
ipinmi <ipinmiadebayo@gmail.com>
2022-09-15 15:55:11 -0700

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_triangle - the entry point
+ * Description: prints a triangle
+ *@size: size of triangle
+ * Return: always 0
+ */
+void print_triangle(int size)
+{
+	int hashes;
+	int spaces;
+
+	if (size > 0)
+	{
+		for (hashes = 1; hashes <= size; hashes++)
+		{
+			for (spaces = 1; spaces <= size; i++)
+			{
+				if (spaces <= size - hashes)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
