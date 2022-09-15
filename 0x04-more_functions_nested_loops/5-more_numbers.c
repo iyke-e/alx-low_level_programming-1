9aa99d66fa725c8fc828f63f94f8f5a7675ad79c
ipinmi <ipinmiadebayo@gmail.com>
2022-09-15 12:19:27 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,10 +13,17 @@ void more_numbers(void)
 	{
 		for (i = 0; i < 15; i++)
 		{
-			_putchar(i + '0');
+			if (i < 10)
+			{
+				_putchar(i + '0');
+			}
+			else
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
 		}
 		_putchar('\n');
 		rep++;
 	}
-	_putchar('\n');
 }
