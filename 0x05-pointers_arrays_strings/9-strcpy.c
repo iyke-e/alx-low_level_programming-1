118f767b987f4e56e87d53904d2f0565e14fe5c4
ipinmi <ipinmiadebayo@gmail.com>
2022-09-19 13:36:32 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * *_strcpy - the entry point
+ *
+ * Description: prints n elements
+ *
+ * @dest: buffer pointer
+ * @src: pointer string
+ *
+ * Return: always 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int len = _strlen(str);
+
+	for (i = 0; i <= len, i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
