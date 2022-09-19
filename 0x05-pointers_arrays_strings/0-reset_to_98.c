871b99969e2c4f602f1e8075d6e390115c6c590c
ipinmi <ipinmiadebayo@gmail.com>
2022-09-19 08:26:38 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - the entry point
+ *
+ * Description: points to an integer
+ *
+ * @n: pointer integer
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
