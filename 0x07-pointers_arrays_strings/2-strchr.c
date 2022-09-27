f96800897ae48d776be3ed976999355a8bc0074d
ipinmi <ipinmiadebayo@gmail.com>
2022-09-26 17:51:57 -0700

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_memcpy - the main entry point
+ * Description: function that locates a character in a string
+ * @s: character pointer
+ * @c: char to find
+ * Return: a pointer to the first occurrence of the character c
+ * in the string s, or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0' ; s++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	if (s[i] == c)
+	{
+		return (s + i);
+	}
+	return (0);
+}
