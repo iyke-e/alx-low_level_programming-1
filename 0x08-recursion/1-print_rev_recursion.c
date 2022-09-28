17c9aec9140dd8f5d7c24e84f7cba9809f3749ce
ipinmi <ipinmiadebayo@gmail.com>
2022-09-28 14:22:27 -0700

    Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - the main entry point
+ * Description: prints a string in reverse
+ * @s: pointer
+ * Return: string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		/* calls the next address space until it reach \0 */
+		/* creating a stack starting from s + n down to s */
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
