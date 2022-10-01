906ef1c927c681770e7a7e460a894318fa31573b
ipinmi <ipinmiadebayo@gmail.com>
2022-09-30 18:55:27 -0700

    Static libs

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,21 +1,17 @@
 #include "main.h"
 /**
  * _puts - the entry point
- *
  * Description: prints a string, followed by a new line
- *
  * @str: pointer string
- *
  * Return: always 0
  */
 void _puts(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; *(str + i) != '\0'; i++)
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(*(str + i));
 	}
 	_putchar('\n');
 }
