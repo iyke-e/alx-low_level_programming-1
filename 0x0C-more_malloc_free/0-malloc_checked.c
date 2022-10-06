5b4c134f0ffe9c1a0def21940674e64917d99be1
ipinmi <ipinmiadebayo@gmail.com>
2022-10-06 10:56:40 -0700

    more malloc and free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *malloc_checked - that allocates memory using malloc.
+ * @b: cast type and size
+ * Return: a pointer to the allocated memory
+ *         Otherwise - malloc fails, the malloc_checked function should
+ * cause normal process termination with a status value of 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit (98);
+	}
+	return (ptr);
+}
